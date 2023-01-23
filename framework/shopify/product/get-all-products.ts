d5b881cd616341bfa6c0579a0a2bdb370ecd761b
Passyah-studioalva <passyah@studioalva.co>
2023-01-23 14:01:21 +0700

    Integrate Custom API Server

--- framework/shopify/product/get-all-products.ts
+++ framework/shopify/product/get-all-products.ts
@@ -1,7 +1,26 @@
-const getAllProducts = async (): Promise<number[]> => {
-  const products = [1, 2, 3];
+const fetchApi = async () => {
+  const url = "https://jsonplaceholder.typicode.com/todos";
 
-  return products;
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await res.json();
+  // Example 1 Recomendeed
+    return { data };
+  // Example 2
+//   return data;
+};
+
+const getAllProducts = async (): Promise<any[]> => {
+  const products = await fetchApi();
+  // Example 1 Recomendeed
+    return products.data;
+  // Example 2
+//   return products;
 };
 
 export default getAllProducts;
