a13276766867f628d96426472b0b16d371862ea0
Passyah-studioalva <passyah@studioalva.co>
2023-01-26 16:03:13 +0700

    update learn ts config specific setup

--- framework/common/config.js
+++ framework/common/config.js
@@ -1,4 +1,3 @@
-const path = require("path");
 /* 
 deepmerge can be used directly in the browser without the use of package managers/bundlers as well:
 UMD version from unpkg.com.
@@ -12,9 +11,11 @@ The ESM entry point was dropped due to a Webpack bug.
 https://www.npmjs.com/package/deepmerge
 */
 const merge = require("deepmerge");
+const fs = require("fs");
+const path = require("path");
 
 function withFrameworkConfig(defaultConfig = {}) {
-  const framework = "bigcommerce";
+  const framework = "shopify";
 
   const frameworkNextConfig = require(path.join(
     "../",
@@ -22,6 +23,13 @@ function withFrameworkConfig(defaultConfig = {}) {
     "next.config"
   ));
   const config = merge(defaultConfig, frameworkNextConfig);
+  const tsPath = path.join(process.cwd(), "tsconfig.json");
+  const tsConfig = require(tsPath);
+
+  tsConfig.compilerOptions.paths["@framework"] = [`framework/${framework}`];
+  tsConfig.compilerOptions.paths["@framework/*"] = [`framework/${framework}/*`];
+
+  fs.writeFileSync(tsPath, JSON.stringify(tsConfig, null, 2));
 
   return config;
 }
