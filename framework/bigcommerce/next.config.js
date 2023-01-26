b0f50424a70f28ba4af8692967f7c47a9ecdb746
Passyah-studioalva <passyah@studioalva.co>
2023-01-26 11:04:28 +0700

    update learn next config file

---
+++ framework/bigcommerce/next.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  test: "bigcmommerce-unique-settings",
+};
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  i18n: {
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US",
+  },
+};
+
+console.log("next.config.js", JSON.stringify(module.exports, null, 2));
