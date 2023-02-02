c4c1fa9b1fec65e75c8285b6709bb534e246b4fb
unknown <passyah@studioalva.co>
2023-02-02 17:53:47 +0700

    reusable layout

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,6 +1,21 @@
+/* Example 1 */
+// import { FC } from "react"
+
+// const Layout:FC = ({ children }:any) => {
+//     return (
+//         <div className="layout">
+//             { children }
+//         </div>
+//     )
+// }
+
+// export default Layout
+
+
+/* Example 2 */
 import { FC } from "react"
 
-const Layout:FC = ({ children }:any) => {
+const Layout:FC = ({ children }) => {
     return (
         <div className="layout">
             { children }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,53 @@
+/* Example 1 */
+// import { AppProps } from "next/app";
+// import { Layout } from "@components/common";
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return (
+//     <>
+//       <Layout>
+//         <Component {...pageProps} />
+//       </Layout>
+//     </>
+//   );
+// }
+
+// export default MyApp;
+
+/* Example 2 */
+// import { AppProps } from "next/app";
+
+// const Layout = (props: any) => {
+//   // 👈️ turns off type checking for props
+//   return <div>{props.children}</div>;
+// };
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return (
+//     <>
+//       <Layout>
+//         <Component {...pageProps} />
+//       </Layout>
+//     </>
+//   );
+// }
+
+// export default MyApp;
+
+/* Example 3 */
 import { AppProps } from "next/app";
+import { FC } from "react";
 
-const Layout = (props: any) => {
-  // 👈️ turns off type checking for props
-  return <div>{props.children}</div>;
-};
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps & { Component: { Layout: FC } }) {
+  const Layout = Component.Layout;
 
-function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
