86c8d9ba7902e020685a8fbb07ebdc8488b487d1
Passyah Raffi <passyah@studioalva.co>
2023-08-22 09:17:26 +0700

    update learn grid component

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -1,15 +1,34 @@
-import { Product } from "@common/types/product"
-import { FC } from "react"
+import { Product } from "@common/types/product";
+import { FC } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 interface Props {
-  product: Product
+  product: Product;
 }
 
-const ProductCard: FC <Props>= ({product}) => {
-
+const ProductCard: FC<Props> = ({ product }) => {
+  const placeholderImage = "/product-image-placeholder.svg";
   return (
-    <div>{product.name}</div>
-  )
-}
+    <Link href={`/products/${product.slug}`}>
+      <div>
+        <h3>
+          <span>{product.name}</span>
+          <span>14 $</span>
+          {product.images && (
+            <Image
+              alt={product.name ?? "Product Image"}
+              src={placeholderImage}
+              height={540}
+              width={540}
+              quality={"85"}
+              layout="responsive"
+            />
+          )}
+        </h3>
+      </div>
+    </Link>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
